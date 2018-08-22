15eb9f5d2a99d8e9f9883b741b3d1be041a7bcc0
mark_drastrup <mark.drastrup@gmail.com>
2018-08-22 21:31:43 +0200

    Adding validation

--- js/validation.js
+++ js/validation.js
@@ -23,46 +23,7 @@ $(document).ready(function () {
             email: "required",
             repeat_email: "required"
         },
-        /*messages: {
-            firstname: formMessages[chosenLanguage].firstname,
-            lastname: formMessages[chosenLanguage].lastname,
-        },*/
     });
-
-    $(".flag-icon").click(function (e) {
-        //get the chosen language
-        let language = $(this).data("lang");
-
-        var inputs = document.getElementsByTagName("input");
-
-        for (var i = 0; i < inputs.length; i++) {
-            if (inputs[i].type !== "hidden") {
-                var demo = inputs[i].id
-                $("#" + demo).rules("add", {
-                    messages: {
-                        required: formMessages[language].demo
-                    }
-                });
-            }
-        }
-
-        /*$("#firstname").rules("add", {
-            messages: {
-                required: formMessages[language].firstname
-            }
-        });
-        $("#lastname").rules("add", {
-            messages: {
-                required: formMessages[language].lastname
-            }
-        });*/
-    });
-
 });
 
-/*messages: {
-    firstname: "Please fill out your firstname",
-    lastname: "Please fill out your lastname",
-},
-errorElement: "div",
-errorLabelContainer: ".has_error"*/
+
